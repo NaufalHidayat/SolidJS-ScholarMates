a75b0a4cee8856e459948164bffdbeb21941655d
NaufalHidayat <90307751+NaufalHidayat@users.noreply.github.com>
2023-06-29 23:45:03 +0700

    Add utils for Front-End

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import Println from "./Debug";
+import { getCookie, checkCookie } from "./Cookie";
+import { getStorage, setStorage, deleteStorage } from "./LocalStorage";
+
+export { Println, getCookie, checkCookie, getStorage, setStorage, deleteStorage };
